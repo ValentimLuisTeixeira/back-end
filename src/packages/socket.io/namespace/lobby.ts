04a60558727a2c4890ea3d9985438cc58d5a1df8
ValentimLuisTeixeira <103111438+ValentimLuisTeixeira@users.noreply.github.com>
2022-06-08 14:14:40 +0100

    f

--- src/packages/socket.io/namespace/lobby.ts
+++ src/packages/socket.io/namespace/lobby.ts
@@ -21,49 +21,32 @@ const memory:Array<User> = [];
 space.on('connection', (socket) => {
     console.log(spaceName,'Connected', socket.id);
 
-   
-    
+    socket.broadcast.emit('new-join', socket.id);
+    /**
+     * Client request to join a specific room
+     */
+    socket.on('join', (data: { room: string }) => {
+        socket.join(data.room);
+        console.log('Client id', socket.id,'has joined room name',data.room);
+        // This emits data to everyone exluding socket it self
+        socket.broadcast.emit('new-join', socket.id);
+    });
 
-      
-
-        socket.on('join', (data:{room: string}) => {
-            socket.data = {
-                idade: new Date(),
-                indentification:uuid.createUUIDv4(),
-            };
-
-            const token = jwt.sign({
-                payload: socket.data,
-                 secretOrPrivateKey: 'gato',
-                 options:{
-                 algorithm:'HS512' ,
-                 expiresIn:'1h'
-                }});
-            socket.join(data.room);
-            socket.emit('toma-la', token);
-
-
-
-       /**
-         * Ao mesmo tempo, emitir ao cliente novo room-data a informaçao do proprio room
-         */
-
-
-       
-        const listSockets = [ ...new Set(space.adapter.rooms.get(data.room)?.values()) ];
-        const socketDatas: Array<any> = [];
-        listSockets.forEach((socketId) => {
-            const socket = space.sockets.get(socketId);
-            socketDatas.push(socket?.data);
-        });
-        socket.emit('room-data', socketDatas);
-            space.emit('novo-user',socket.data)
-
-        });
+    socket.on('socket-list', (data: { room: string }) => {
+        console.log(space.adapter.rooms.get(data.room)?.values());
+        socket.emit(
+            'socket-list', 
+                [ 
+                    ... new Set(space.adapter.rooms.get(data.room)?.values())
+                ]
+            );
+    });
 
 
     socket.on('disconnect', (reason) => {
         console.log(spaceName,'Disconnected', socket.id, reason);
-        socket.broadcast.emit('user_Saiu',socket.data);
+        //space.emit('user-bazou', socket.data);
+        socket.broadcast.emit('new-join', socket.id);
     });
+
 });
